b293c05d387af0b0e1ff828ca1bd298b4499d77b
noussel <noussaibaelkhadar@gmail.com>
2026-02-11 19:49:15 +0000

    add : classe architecture

--- c09/ex02/PmergeMe.hpp
+++ c09/ex02/PmergeMe.hpp
@@ -1,16 +1,38 @@
 #pragma once
 #include <iostream>
-#include <vecter>
+#include <vector>
+#include <deque>
+#include <set>
 
-template <typename T>
+
 class  PmergeMe{
     private :
-        T container;
+        std::vector<int> vec;
+        std::deque<int>  deq;
+
     public :
         PmergeMe();
-        PmergeMe(PmergeMe &other);
-        PmergeMe &operator=(PmergeMe &other);
-        void fill<T>();//check for duplication (usint std::set)& fill out the container 
-        void FordJohnson<T>();//sorting algorithme -> create paires -> put max and min -> sort max values -> put min using binary search
+        PmergeMe(const PmergeMe &other);
+        PmergeMe &operator=(const PmergeMe &other);
         ~PmergeMe();
+
+        void parseInput(char **av);//check for duplication (usint std::set)& fill out the container 
+
+        template <typename container> 
+        void FordJohnson(container &c);//sorting algorithme -> create paires -> put max and min -> sort max values -> put min using binary search
+};
+
+void PmergeMe::parseInput(char **av){
+    std::set<int> s;
+    // Créer un std::set<int> seen;
+
+    // Pour chaque argument :
+    // 1. Vérifier que c’est un nombre valide
+    // 2. Convertir en int
+    // 3. Vérifier overflow / négatif
+    // 4. Essayer seen.insert(n)
+    //    - si false → Error (doublon)
+    // 5. Ajouter n dans vec
+    // 6. Ajouter n dans deq
+
 }
